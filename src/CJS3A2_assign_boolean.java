4ba02f14dc2a5e4e605f9b6c4de57b32c12f1e97
PavanRangudu <pavanharishankar@gmail.com>
2017-03-14 20:53:02 +0530

    assign_true_or_false_in_single_statement

---
+++ src/CJS3A2_assign_boolean.java
@@ -0,0 +1,11 @@
+
+public class CJS3A2_assign_boolean {
+	public static void main(String []s){
+		String S1 = "first";
+		String S2 = "second";
+		boolean same = (S1==S2) ? true : false;
+		
+		System.out.println(same);  // Output will be "false"
+	}
+
+}
